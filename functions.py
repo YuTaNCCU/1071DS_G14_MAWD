03c456dd3f29255a38126fb101d8d9981e134eb8
Alice Tsai <alice90270@gmail.com>
2019-01-21 03:47:17 +0800

    change: testing data, capacityDifference, courseArrangement
    1. chage testing data int strings
    2. change courseArrangement's if condition as strings instead of int
    '' as default
    3. calculate capacityDifference as percentage and translate into 0~100 score
    need max, min value of student number limitation info

--- functions.py
+++ functions.py
@@ -7,51 +7,51 @@ k=weekdays*roomNum #15
 # testing data
 """
 schedule=
-['',
- 1,
- 2,
- 3,
- 4,
- 5,
- 6,
+['306000001',
+ '',
+ '306008001',
  '',
  '',
+ '306016002',
+ '356425001',
  '',
+ '306016012',
+ '307873001',
  '',
+ '307867001',
+ '307942001',
  '',
- 12,
- 13,
- 14,
+ '307870001',
+ '306016022',
+ '307857001',
+ '306050011',	
+ '356387001',	
+ '356388001',	
+ '307851001',	
+ '306525001',
  '',
- 16,
- 17,
- 18,
- 19,
+ '307035001',	
+ '306736001',	
+ '356395001',	
+ '307034001',	
+ '356461001',
  '',
- 21,
- 22,
+ '306737001',
+'307932001',
+'',
+ '356822001',
  '',
- 24,
- 25,
- 26,
- 27,
- 28,
- '',
- 30,
- 31,
- 32,
- 33,
- 34,
- '',
- 36,
- 37,
- 38,
- '',
- 40,
- 41,
- 42,
- 43,
- '']
+'356389001',	
+'356019001',	
+'356564001',
+'',
+'307901001',	
+'356813001',
+'',
+'356808001',
+'',
+'',
+'']
 """
 
 #initial schedule
@@ -69,16 +69,32 @@ def sessionDispersion():
     print("dispersion")
 
 #3 教室與人數有剛好match    
-def capacityDifference(rCapacity,cCapacity):
-    d=abs(rCapacity-cCapacity)
-    print("d")    #要想一個更好的量化方式嗎？還是就以差值為主？
-    
+"""
+Input: 
+教室人限rCapacity(number), 課程人限cCapacity(number)
+教室人限最大值rmax(number), 最小值rmin(number)
+課程人限最大值cmax(number), 最小值cmin(number)
+Output: 人限差距分數dscore(number) 0~100分
+"""
+def capacityDifference(rCapacity,cCapacity,rmax,rmin,cmax,cmin):
+    dividends=max(rmax-cmin, cmax-cmin)
+    d=abs(rCapacity-cCapacity)    
+    if d==0:
+    	dscore=100
+    else:
+    	dscore=100-(d/dividends*100) #距離越大分數越低
+    dscore   
+
 #4 課程數量：下午>早上>晚上
+"""
+Input: 一維度課表schedule(list), 計算索引的參數k(number)
+Output: 各時段的課程數量加總periodSum(list) 
+"""
 def courseArrangement(schedule, k):
 	periodSum=[]
 	temp=0
 	for i, x in enumerate(schedule):
-	    if x!=i: #有課
+	    if x!='' #有課
 	        temp=temp+1
 	        print(i,'TRUE')
 	    else:    #沒課
