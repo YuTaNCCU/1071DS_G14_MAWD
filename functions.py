1b88c3f9bd6a482eab6ae7957ff8f523e1086459
Alice Tsai <alice90270@gmail.com>
2019-01-21 16:39:44 +0800

    Revert "adjust rule4"
    This reverts commit 1b2e88a5872fbe697a8e9d07179c59a073cc466b.

--- functions.py
+++ functions.py
@@ -5,7 +5,7 @@ dailyParts=3 #parts=將一天劃分為[早上、下午、晚上]
 period=weekdays*dailyParts #15 一個weekdays中，不分教室的區塊總數
 session=roomNum*weekdays*dailyParts #45 一個weekdays中，空教室的總數(一維陣列的長度)
 k=weekdays*roomNum #15 [早上、下午、晚上] 一個part中的session數(索引調整參數)
-# In[] 
+
 # testing data
 """
 schedule=['306000001','','306008001','','','306016002','356425001','','306016012','307873001','','307867001','307942001','','307870001',
@@ -17,7 +17,7 @@ schedule=['306000001','','306008001','','','306016002','356425001','','306016012
 schedule=[]
 for i in range(session):
     schedule.append('')
-# In[] 
+
 #1 教授單日授課集中度(例如：老師一天從早上直接上課到晚上)
 """
 Input: schedule(list), 含授課教師課程細節的courseDetail(dictionary)
@@ -45,7 +45,7 @@ def dailyConcentration(schedule, courseDetail):
 		    	pass
     dailyConc
 
-# In[] 
+
 #2 每段時間的課程離散度(取各Period課程數的平方和計算)
 """
 一個時段三堂課、一個時段兩堂課、一個時段一堂課
@@ -74,7 +74,7 @@ def sessionDispersion(schedule, roomNum):
 	###
 	sdisp=(squaresum-mindiv)/dividends*100 #量化為0~100分
 	sdisp
-# In[] 
+
 #3 教室與人數有剛好match    
 """
 Input: 
@@ -90,43 +90,24 @@ def capacityDifference(rCapacity,cCapacity,rmax,rmin,cmax,cmin):
     	cdiffscore=100
     else:
     	cdiffscore=100-(cdiff/dividends*100) #算出差距的百分比，距離越大分數越低
-    return cdiffscore   
+    cdiffscore   
 
-# In[] 
+
 #4 課程數量：下午>早上>晚上
 """
 Input: 一維度課表schedule(list), 計算索引的參數k(number)
-Output: 下午period數所佔比例
+Output: 各時段的課程數量加總periodSum(list) 
 """
 def courseArrangement(schedule, k):
 	periodSum=[]
 	temp=0
 	for i, x in enumerate(schedule):
-		if x!='': #有課
-			temp=temp+1
-		if(i%k==k-1):
-			periodSum.append(temp)
-			temp=0
-	periodSum  #course number list [morning, afternoon, eveneing]
-	weights=[0.3, 0.5, 0.2]
-	result=0
-	for i, p in enumerate(periodSum):
-	    result=result+p*weights[i]
-	return (result/sum(periodSum)*100)  #加權period數除以全部period數，0~100分越大越好
-
-schedule=['306000001','','','','','','','','','','','','','','',
- '','307857001','','','','','','','','','356395001','','','','306737001',
-'307932001','','356822001','','','','','','','','','','','','']
-
-# In[] 執行函數
-#3
-rCapacity=40
-cCapacity=60
-rmax=80
-rmin=60
-cmax=40
-cmin=25
-capacityDifference(rCapacity,cCapacity,rmax,rmin,cmax,cmin)#
-#4
-courseArrangement(schedule, k)
+	    if x!='' #有課
+	        temp=temp+1
+	        print(i,'TRUE')
+	    else:    #沒課
+	        print(i,'FALSE')
+	    if(i%k==k-1):
+	        periodSum.append(temp)
+	periodSum #course number list [morning, afternoon, eveneing]
 
