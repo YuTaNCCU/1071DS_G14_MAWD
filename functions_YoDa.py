8476446fcde7a370b621869bf560d44c0c47c904
YuTaNCCU <ayuttyua@gmail.com>
2019-01-22 15:58:27 +0800

    repair

--- functions_YoDa.py
+++ functions_YoDa.py
@@ -183,9 +183,12 @@ ObjFun(schedule,courseDetail, roomNum, k, RoomDetail)
 
 
 def ObjFun(schedule,courseDetail, roomNum, k, RoomDetail, session, period, totalCourseNum):
-	weight=[50, 25, 15, 5]
-	value=weight[0]*dailyConcentration(schedule, courseDetail)+weight[1]*sessionDispersion(schedule, roomNum, session, period, totalCourseNum)+weight[2]*capacityDifference(schedule, RoomDetail)+weight[3]*courseArrangement(schedule, k)
-
+    weight=[50, 25, 15, 5]
+    value=weight[0]*dailyConcentration(schedule, courseDetail)+\
+    weight[1]*sessionDispersion(schedule, roomNum, session, period, totalCourseNum)+\
+    weight[2]*capacityDifference(schedule, RoomDetail)+\
+    weight[3]*courseArrangement(schedule, k)
+    return value
 
 ObjFun(schedule,courseDetail, roomNum, k, RoomDetail, session, period, totalCourseNum)
 
